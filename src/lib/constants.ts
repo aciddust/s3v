5430d09012a4ca8f734f7d31402642d2d9597992
d3fau1t <aciddust@users.noreply.github.com>
2026-04-13 19:49:55 +0900

    chore: bump v0.4.5

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = 'S3V';
-export const APP_VERSION = '0.4.4';
+export const APP_VERSION = '0.4.5';
 export const APP_DESCRIPTION = 'S3-Compatible Storage Client';
 export const SUPPORT_EMAIL = 'acidlab.help@gmail.com';
 export const FEEDBACK_API = 'https://web.d3fau1t.net/api/feedback';
